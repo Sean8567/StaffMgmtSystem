e146c1e9609c8efabf7d903d70e88948ec808db0
Sean8567 <asd12322612@gmail.com>
2021-04-23 23:03:00 +0800

    StaffMgmtSystem

---
+++ src/main/java/com/mds/controller/StaffManagementController.java
@@ -0,0 +1,13 @@
+package com.mds.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class StaffManagementController {
+    @RequestMapping("/{name}")
+    public String greeting(@PathVariable("name") String name) {
+        return "Hello, " + name;
+    }
+}
